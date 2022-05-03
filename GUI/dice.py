f5d6e91cba962ba8cf802fb375770c2e2dfbfb1a
andrewyanggreet <andrewseanyang0213@gmail.com>
2022-05-03 18:39:00 -0400

    rename

---
+++ GUI/dice.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+def init_dice():
+    # /Users/andrewyang/Desktop/Spring Semester/Randomness/dice-game/GUI/
+    imagePath = "/Users/andrewyang/Desktop/Spring Semester/Randomness/dice-game/GUI/imgs/"
+
+    diceList = []
+
+    dice1 = Image.open(imagePath + "one.png").resize((50, 50))
+    dice2 = Image.open(imagePath + "two.png").resize((50, 50))
+    dice3 = Image.open(imagePath + "three.png").resize((50, 50))
+    dice4 = Image.open(imagePath + "four.png").resize((50, 50))
+    dice5 = Image.open(imagePath + "five.png").resize((50, 50))
+    dice6 = Image.open(imagePath + "six.png").resize((50, 50))
+
+    diceList.append(ImageTk.PhotoImage(dice1))
+    diceList.append(ImageTk.PhotoImage(dice2))
+    diceList.append(ImageTk.PhotoImage(dice3))
+    diceList.append(ImageTk.PhotoImage(dice4))
+    diceList.append(ImageTk.PhotoImage(dice5))
+    diceList.append(ImageTk.PhotoImage(dice6))
+    return diceList
